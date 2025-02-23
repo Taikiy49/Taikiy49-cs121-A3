1e7268350d932d1a03e44f60f1f855fbc2068666
Ian <Iannakashima8@gmail.com>
2025-02-23 12:15:57 +0000

    added the file parsing.

--- m1.py
+++ m1.py
@@ -114,6 +114,22 @@ class SearchEngine:
 
         ranked_docs = sorted(doc_scores.items(), key=lambda x: x[1], reverse=True)
         return ranked_docs[:5]  # this will return the top 5 results
+    
+    def read_file(self,file_input):
+        file_size = os.path.getsize('inverted_index.json')
+        stop_words = ('a','about','above','after','again','against','all','am','an',"and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've", "your",'yours','yourself','yourselves')
+        with open(file_input, 'r') as file:
+            data = json.load(file)
+        total_docs = data['total_docs']
+        data_list_words = list(data['doc_freq'])
+        count = 0
+        for words in data_list_words:
+            if(words not in stop_words):
+                count += 1
+        print('Total Number of Docs:',total_docs)
+        print("Number of Unique words:",count)
+        print("The size of the file is: ", file_size/1024, "KB")
+
 
 # okay now let's test this
 dataset_path = 'test-directory'  # gonna make sure this directory actually exists
@@ -121,9 +137,11 @@ search_engine = SearchEngine(dataset_path)
 search_engine.build_index()  # gonna build the index first
 search_engine.load_index()   # now we load it
 
+
 query = "machine learning"  # first test query, gotta test more later
 results = search_engine.search(query)
-file_size = sys.getsizeof('inverted_index.json')
+ 
+
 
 print("the top 5 results are: ", results)
-print("The size of the file is: ", file_size/1000)
+search_engine.read_file('inverted_index.json') 
